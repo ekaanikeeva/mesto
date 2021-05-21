f7333c1862bc46911cfb9f34a13b5d8168d5924d
Ekaterina Anikeeva <ekaanikeeva@yandex.ru>
2021-05-21 12:02:22 +0300

    fix: правки по ревью

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,38 @@
 let profile = document.querySelector('.profile');
-let openPopup = profile.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
-let savePopup = popup.querySelector('.popup__btn-save');
-let closePopup = popup.querySelector('.popup__btn-close');
-let profileName;
-let popupName;
-let profileStatus;
-let popupStatus;
+let form = document.querySelector('.form');
+let openPopupBtn = profile.querySelector('.profile__edit-btn');
+let savePopupBtn = form.querySelector('.form__btn-save');
+let closePopupBtn = popup.querySelector('.popup__btn-close');
 
-profileName = document.getElementById('profile__name');
-popupName = document.getElementById('popup__name');
-popupName.value = profileName.textContent;
+let profileName = document.getElementById('profile__name');
+let popupName = document.getElementById('popup__name');
+let profileStatus = document.getElementById('profile__status');
+let popupStatus = document.getElementById('popup__status');
+    
 
-profileStatus = document.getElementById('profile__status');
-popupStatus = document.getElementById('popup__status')
-popupStatus.value = profileStatus.textContent;
-
-savePopup.addEventListener('click', savePopupChanges);
-openPopup.addEventListener('click', activePopup);
-closePopup.addEventListener('click', activePopup);
-
-
+//открытие попап и отправка данных из профиля в форму
 function activePopup () {
-    popup.classList.toggle('popup_opened');
+    popupName.value = profileName.textContent;
+    popupStatus.value = profileStatus.textContent;
+    popup.classList.add('popup_opened');
 }
 
-function savePopupChanges() {
+
+//закрытие попап
+function closePopupForm () {
+    popup.classList.remove('popup_opened');
+}
+
+//сохраняет изменения popup
+function savePopupChanges (submit) {
+    submit.preventDefault();
     profileName.textContent = popupName.value;
     profileStatus.textContent = popupStatus.value;
-    activePopup();
+    closePopupForm();
 }
 
+
+openPopupBtn.addEventListener('click', activePopup);
+closePopupBtn.addEventListener('click', closePopupForm);
+form.addEventListener('submit', savePopupChanges);
